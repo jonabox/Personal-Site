1d370c99a23b921d46c8653e768308d3fe211854
jesteban <jesteban@mit.edu>
2020-02-08 17:34:06 -0500

    starter code for personal website

---
+++ layouts/default.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-app style="background: linear-gradient(45deg, #DF4FF8 0%, #0091A0 100%);">
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+      bottom
+      color="rgb(0, 0, 0, 0.7)"
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+      color="rgb(0, 0, 0, 0.7)"
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title v-text="title"/>
+      <v-spacer />
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer
+      :fixed="fixed"
+      app
+    >
+      <span>&copy; 2020 Jonathan Esteban</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: true,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspiration',
+          to: '/inspiration'
+        },
+        {
+          icon: 'mdi-hammer-wrench',
+          title: 'Projects',
+          to: '/projects'
+        }
+      ],
+      miniVariant: false,
+      title: 'jonabox'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-layout>
+    <v-flex>
+      <v-card outlined color="rgb(0, 0, 0, 0.1)" class="my-3">
+                <v-card-text class="display-3 font-weight-thin text-center" style="word-break: normal">Welcome to my homepage</v-card-text>
+      </v-card>
+
+      <v-card outlined color="rgb(0, 0, 0, 0.1)" class="my-3">
+        <v-card-title>Education</v-card-title>
+        <v-card-subtitle>Massachusetts Institute of Technology</v-card-subtitle>
+        <v-card-text>
+          <p> Candidate for Masters of Engineering Program in Computer Systems</p> 
+          <p> Bachelor of Science in Computer Science, 2019</p> 
+        </v-card-text>
+      </v-card>
+
+      <v-card outlined color="rgb(0, 0, 0, 0.1)" class="my-3">
+        <v-card-title>Work and Research</v-card-title>
+        <v-card-text>
+          <h4 class="display-7 font-weight-thin">Multi-Party Computation Project</h4>
+          <ul>
+            <li>Designed the foundation for a web platform that would encapsulate a bare-bones C++ implementation of several cryptographic and multi-party computations</li>
+            <li>Worked on transcribing the C++ codebase into JavaScript using Emscripten, a tool that compiles C++ into WebAssembly</li>
+            <li>Setup a MySQL database for the storage of credentials and an SMTP server to handle email tokens</li>
+            <li>Followed many security best practices, including salting/hashing passwords, CSRF tokens, CSP headers, and creating TLS certificates using Let'sEncrypt</li>
+          </ul>
+        </v-card-text>
+
+        <v-card-text>
+          <h4 class="display-7 font-weight-thin">Vivint SmartHome Internship</h4>
+          <ul>
+            <li>Worked on circumventing corporate firewalls using NAT traversing servers for WebRTC applications</li>
+            <li>Used Docker, Kubernetes, Terraform, and Google’s Cloud Platform to automate the setup of said servers and scale the infrastructure</li>
+          </ul>
+        </v-card-text>
+
+        <v-card-text>
+          <h4 class="display-7 font-weight-thin">Cells for Cells Internship</h4>
+          <ul>
+            <li>Worked with a team of biotech investigators and facilitated CAD design, 3D printing, and Arduino circuit logistics to design a syringe pump embedded robotic arm to be used for precise bio-filament placement</li>
+          </ul>
+        </v-card-text>
+      </v-card>
+
+      <v-card outlined color="rgb(0, 0, 0, 0.1)" class="my-3">
+        <v-card-title>Skills</v-card-title>
+        <v-card-text>Fully Bilingual (Spanish, English); Proficiency in Python, Java, JavaScript; Experience with AngularJS, Rust, Express, VueJS, SQL, cloud development (GCP, AWS EC2, Docker, Terraform), Git, HTML, CSS, and C++; Familiar with React, MongoDB, AutoCAD, Unity2D, Android Studio, Photoshop, FL Studio, 2D animation, video editing, 3D printing, Arduino systems, electronic tools, Unix systems, Redis, Gstreamer, WebRTC, FFmpeg</v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+import VuetifyLogo from "~/components/VuetifyLogo.vue";
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+};
+</script>

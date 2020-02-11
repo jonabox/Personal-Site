6c707ccb7d7e4e26c8cb6d8414ad779209213466
jesteban <jesteban@mit.edu>
2020-02-11 12:20:41 -0500

    added projects

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,19 +1,58 @@
 <template>
   <v-layout>
-    <v-flex class="text-center">
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
+    <v-flex>
+      <v-card outlined color="rgb(0, 0, 0, 0.1)" class="my-3">
+        <v-card-text class="display-3 font-weight-thin text-center" style="word-break: normal">Projects I have worked on</v-card-text>
+      </v-card>
+
+      <v-card outlined color="rgb(0, 0, 0, 0.1)" class="my-3">
+        <v-card-title>Pizza-Partner</v-card-title>
+        <v-card-subtitle>MIT Web Lab Project (2020)</v-card-subtitle>
+        <v-card-text>
+          <p> A webiste that lets students partner up with other students within the same dorm so that they can make a group Domino's order and can save money. Pizza Partner achieves this by automatically applying coupons to the combined order.</p> 
+          <p> Made with Vue (frontend), Vuetify (ui-toolkit), SQLite (database) Stripe.js (payments)</p> 
+        </v-card-text>
+      </v-card>
+
+      <v-card outlined color="rgb(0, 0, 0, 0.1)" class="my-3">
+        <v-card-title>Multi-Party Computation Web Platform </v-card-title>
+        <v-card-subtitle>MIT Research Program (2019) </v-card-subtitle>
+        <v-card-text>
+          <p> Web platform that allows for the  multi-party computation of cryptographic functions</p> 
+          <p> Made with Vue (frontend), BootstrapVue (ui-toolkit), Let'sEncrypt (TLS certificates)</p> 
+        </v-card-text>
+      </v-card>
+      
+      <v-card outlined color="rgb(0, 0, 0, 0.1)" class="my-3">
+        <v-card-title>Mentorly</v-card-title>
+        <v-card-subtitle>MIT 6.170 Software Studio Project (2018)</v-card-subtitle>
+        <v-card-text>
+          <p> Class final project website that connects students to mentors in academic and professional disciplines</p> 
+          <p> Made with Vue (frontend), Semantic (frontend-components), SQLite (database)</p> 
+        </v-card-text>
+      </v-card>
+
+      <v-card outlined color="rgb(0, 0, 0, 0.1)" class="my-3">
+        <v-card-title>History Box</v-card-title>
+        <v-card-subtitle>MIT Web Lab Project (2018)</v-card-subtitle>
+        <v-card-text>
+          <p> Website that allows users to view and vote on historic, celebrated photos</p> 
+          <p> Made with Flask and MongoDB</p> 
+        </v-card-text>
+      </v-card>
+
     </v-flex>
   </v-layout>
 </template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+import VuetifyLogo from "~/components/VuetifyLogo.vue";
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+};
+</script>

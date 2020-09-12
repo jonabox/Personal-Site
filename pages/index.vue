67f3f782ba37d308b15bad76ca6d81052197bc3c
Jonathan Esteban <jesteban@mit.edu>
2020-09-12 01:55:33 -0400

    image tweaks

--- pages/index.vue
+++ pages/index.vue
@@ -4,14 +4,14 @@
       <v-card flat color="transparent" class="my-4">
         <v-card-text class="display-4 text-center" style="word-break: normal">Welcome to my site!</v-card-text>
       </v-card>
-
+      <v-img class="center rounded-lg" max-width="1000px" dark :src="require('~/assets/iceland.jpeg')"></v-img>
       <v-card flat color="transparent" class="my-3">
-        <v-img dark :src="require('~/assets/iceland.jpeg')"></v-img>
         <v-card-text>
-          <p class="display-2 text-center">I am a Puerto Rican Master's Student at MIT. I hope to graduate by May 2021 and will be looking for full-time positions in the meantime. Exciting!!!</p>
+          <p
+            class="display-1 text-center"
+          >I am a 🇵🇷 Puerto Rican 🇵🇷 Master's Student at MIT. I hope to graduate by May 2021 and will be looking for full-time positions in the meantime. Exciting!!!</p>
         </v-card-text>
       </v-card>
-
     </v-flex>
   </v-layout>
 </template>
@@ -27,3 +27,12 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  border-radius:10px;
+}
+</style>

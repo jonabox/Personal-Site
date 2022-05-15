7d452bd3b1967f93203cb1539001a963e9f3b143
Jonathan Esteban <jesteban@mit.edu>
2022-05-15 16:28:31 -0400

    added fading page transition

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,43 @@
 <template>
   <v-app style="background: linear-gradient(45deg, #4f0042 0%, #240087 100%)">
-    <v-app-bar dense app clipped-left color="rgb(0, 0, 0, 0.1)" style="backdrop-filter: blur(4px)">
+    <v-app-bar
+      dense
+      app
+      clipped-left
+      color="rgb(0, 0, 0, 0.1)"
+      style="backdrop-filter: blur(4px)"
+    >
       <v-toolbar-title v-text="title" />
       <v-spacer />
       <v-btn large icon href="https://github.com/jonabox">
         <v-icon>mdi-github</v-icon>
       </v-btn>
-      <v-btn large icon href="https://linkedin.com/in/jonathan-esteban-771066138/">
+      <v-btn
+        large
+        icon
+        href="https://linkedin.com/in/jonathan-esteban-771066138/"
+      >
         <v-icon>mdi-linkedin</v-icon>
       </v-btn>
       <v-btn large icon href="mailto:jesteban@mit.edu">
         <v-icon>mdi-email</v-icon>
       </v-btn>
     </v-app-bar>
-    <v-navigation-drawer clipped permanent app :mini-variant="isScreenSmall" color="rgb(0, 0, 0, 0.1)">
-      <v-list >
-        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
+    <v-navigation-drawer
+      clipped
+      permanent
+      app
+      :mini-variant="isScreenSmall"
+      color="rgb(0, 0, 0, 0.1)"
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
@@ -24,7 +46,10 @@
           </v-list-item-content>
         </v-list-item>
 
-        <v-list-item href="https://pizza-partner.herokuapp.com/" target="_blank">
+        <v-list-item
+          href="https://pizza-partner.herokuapp.com/"
+          target="_blank"
+        >
           <v-list-item-action>
             <v-icon>mdi-food</v-icon>
           </v-list-item-action>
@@ -35,15 +60,22 @@
       </v-list>
     </v-navigation-drawer>
     <v-main>
-      <v-container fluid>
+      <v-fade-transition>
         <nuxt />
-      </v-container>
+      </v-fade-transition>
     </v-main>
-    <v-footer class="font-weight-light text-center text-caption" app absolute tile inset color="rgb(0, 0, 0, 0)">
-    Made with Nuxt.js, powered by Netlify
-    <v-spacer></v-spacer>
-    <div>&copy; {{ new Date().getFullYear() }}</div>
-  </v-footer>
+    <v-footer
+      class="font-weight-light text-center text-caption"
+      app
+      absolute
+      tile
+      inset
+      color="rgb(0, 0, 0, 0)"
+    >
+      Made with Nuxt.js, powered by Netlify
+      <v-spacer></v-spacer>
+      <div>&copy; {{ new Date().getFullYear() }}</div>
+    </v-footer>
   </v-app>
 </template>
 
